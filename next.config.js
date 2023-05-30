5d41c85b79a5d2509eb28ad09ca3a645a57a9629
Gustavo Bucker <gb.bucker@gmail.com>
2023-05-30 15:28:30 -0300

    Rem line from next.config.js, clean up of configs

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig = {
   images: {
     domains: ["cdn.sanity.io"],
   },
-  assetPrefix: isProd ? "https://apicdn.sanity.io" : undefined,
 };
 
 module.exports = nextConfig;
